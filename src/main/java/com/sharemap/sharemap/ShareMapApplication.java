42e614578c5f2293f054867ad859446bb8791c34
OniOniOn <0onionion0@gmail.com>
2023-04-26 00:34:28 +0900

    spring init

---
+++ src/main/java/com/sharemap/sharemap/ShareMapApplication.java
@@ -0,0 +1,13 @@
+package com.sharemap.sharemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharemap/sharemap/ShareMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharemap.sharemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
