abb35f51850b7c01377acbc7017de932fdfe1807
Kang Min <rkdals133@gmail.com>
2023-08-22 22:04:50 +0900

    feat : API 응답 클래스 사용

--- src/main/java/zip/ootd/ootdzip/clothes/controller/ClothesController.java
+++ src/main/java/zip/ootd/ootdzip/clothes/controller/ClothesController.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import zip.ootd.ootdzip.clothes.data.ClothesResponseDto;
 import zip.ootd.ootdzip.clothes.data.SaveClothesDto;
 import zip.ootd.ootdzip.clothes.service.ClothesService;
+import zip.ootd.ootdzip.common.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,12 +23,14 @@ public class ClothesController {
     private final ClothesService clothesService;
 
     @Operation(summary = "옷 저장 API", description = "옷 저장 API")
-    @PostMapping
-    public ClothesResponseDto saveClothes(SaveClothesDto saveClothesDto){
-        ClothesResponseDto result = null;
-        result = clothesService.saveClothes(saveClothesDto);
-
-        return result;
+    @PostMapping("/")
+    public ApiResponse<ClothesResponseDto> saveClothes(@RequestBody SaveClothesDto saveClothesDto){
+        ClothesResponseDto clothesResponseDto = null;
+        clothesResponseDto = clothesService.saveClothes(saveClothesDto);
+        return ApiResponse.<ClothesResponseDto>builder()
+                .result(clothesResponseDto)
+                .resultCode(0)
+                .resultMsg("옷 저장 성공").build();
     }
 
 }
