155fb28e6e7b447151e2a446020c61b87823d572
Kang Min <rkdals133@gmail.com>
2023-08-22 22:05:19 +0900

    feat : validation 어노테이션 추가

--- src/main/java/zip/ootd/ootdzip/clothes/data/SaveClothesDto.java
+++ src/main/java/zip/ootd/ootdzip/clothes/data/SaveClothesDto.java
@@ -1,9 +1,12 @@
 package zip.ootd.ootdzip.clothes.data;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -12,27 +15,38 @@ import java.util.List;
 @NoArgsConstructor
 public class SaveClothesDto {
 
+    @NotNull
+    @Positive
     private Long userId;
 
+    @NotBlank
+    @Size(max = 100)
     private String clothesName;
 
+    @NotNull
+    @Positive
     private Long brandId;
 
+    @NotNull
+    @Positive
     private Long categoryId;
 
-    private List<Long> styleIdList;
+    private List<@Positive Long> styleIdList;
 
-    private List<Long> colorIdList;
+    private List<@Positive Long> colorIdList;
 
     private Boolean isOpen;
 
     private String size;
 
+    @NotBlank
     private String material;
 
+    @NotBlank
     private String purchaseStore;
 
+    @NotBlank
     private String purchaseDate;
 
-    private List<String> clothesImageList;
+    private List<@NotBlank String> clothesImageList;
 }
