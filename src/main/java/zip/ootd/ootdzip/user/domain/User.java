f66f84c67e32dd27893f8581b7579e495edbe3c7
Lechros <0onionion0@gmail.com>
2023-08-28 10:16:46 +0900

    feat: follow, unfollow, 쿼리 기능 추가

--- src/main/java/zip/ootd/ootdzip/user/domain/User.java
+++ src/main/java/zip/ootd/ootdzip/user/domain/User.java
@@ -5,6 +5,8 @@ import lombok.*;
 import zip.ootd.ootdzip.common.entity.BaseEntity;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -39,6 +41,25 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private Boolean isDeleted = false;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "followings")
+    private final Set<User> followers = new HashSet<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "followers",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "follower_id")})
+    private final Set<User> followings = new HashSet<>();
+
+    public boolean addFollower(User user) {
+        return followers.add(user)
+                && user.followings.add(this);
+    }
+
+    public boolean removeFollower(User user) {
+        return followers.remove(user)
+                && user.followings.remove(this);
+    }
 
     public static User getDefault() {
         return new User(
