0da1d21d1277493c0f866951653e46c1da54d517
Jinhoon Park <g22203@naver.com>
2023-09-13 16:57:50 +0900

    feat: BoardImage 정적 생성메소드 추가

--- src/main/java/zip/ootd/ootdzip/board/domain/BoardImage.java
+++ src/main/java/zip/ootd/ootdzip/board/domain/BoardImage.java
@@ -1,14 +1,17 @@
 package zip.ootd.ootdzip.board.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import zip.ootd.ootdzip.common.entity.BaseEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "board_images")
 @Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardImage extends BaseEntity {
@@ -19,4 +22,16 @@ public class BoardImage extends BaseEntity {
 
     @Column(length = 2048)
     private String imageUrl;
+
+    public static BoardImage createBoardImageBy(String imageUrl) {
+        return BoardImage.builder()
+                .imageUrl(imageUrl)
+                .build();
+    }
+
+    public static List<BoardImage> createBoardImagesBy(List<String> imageUrls) {
+        return imageUrls.stream()
+                .map(BoardImage::createBoardImageBy)
+                .collect(Collectors.toList());
+    }
 }
