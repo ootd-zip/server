b5325067fcc310d119ee3ba7949fc5b9ae6d3aa2
Jinhoon Park <g22203@naver.com>
2023-09-13 16:57:21 +0900

    feat: style 추가

---
+++ src/main/java/zip/ootd/ootdzip/board/domain/Style.java
@@ -0,0 +1,20 @@
+package zip.ootd.ootdzip.board.domain;
+
+public enum Style {
+
+    GIRLISH,
+    CASUAL,
+    STREET,
+    ROMANTIC,
+    FORMAL,
+    SPORTY,
+    CHIC,
+    AMEKAJI,
+    MINIMAL,
+    GOREPCORE,
+    RETRO,
+    HOMEWARE,
+    OLDMONEY,
+    NEWMONEY,
+    BLOCKCORE,
+}
