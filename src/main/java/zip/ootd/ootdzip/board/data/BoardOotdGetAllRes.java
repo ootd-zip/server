53b500654d36946f69c008b93f97c67ce270b4b6
Jinhoon Park <g22203@naver.com>
2023-09-25 15:28:11 +0900

    feat: 전체 게시글 조회 dto 추가

---
+++ src/main/java/zip/ootd/ootdzip/board/data/BoardOotdGetAllRes.java
@@ -0,0 +1,134 @@
+package zip.ootd.ootdzip.board.data;
+
+import lombok.Data;
+import zip.ootd.ootdzip.board.domain.Board;
+import zip.ootd.ootdzip.board.domain.BoardImage;
+import zip.ootd.ootdzip.boardclothe.domain.BoardClothes;
+import zip.ootd.ootdzip.boardstyle.BoardStyle;
+import zip.ootd.ootdzip.brand.domain.Brand;
+import zip.ootd.ootdzip.category.domain.Category;
+import zip.ootd.ootdzip.clothes.domain.Clothes;
+import zip.ootd.ootdzip.user.domain.UserGender;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class BoardOotdGetAllRes {
+
+    private Long id;
+
+    private boolean isUserLike;
+
+    private String name;
+
+    private String profileImage;
+
+    private String contents;
+
+    private int viewCount;
+
+    private int reportCount;
+
+    private int likeCount;
+
+    private LocalDateTime createAt;
+
+    private UserGender gender;
+
+    private List<OotdBoardImage> boardImages;
+
+    private List<OotdStyle> styles;
+
+    private List<OotdBoardClothesList> boardClothesList;
+
+    public BoardOotdGetAllRes(Board board,
+                              boolean isLike,
+                              int viewCount,
+                              int likeCount) {
+
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+
+        this.id = board.getId();
+        this.isUserLike = isLike;
+        this.reportCount = board.getReportCount();
+        this.contents = board.getContents();
+        this.gender = board.getGender();
+        this.createAt = board.getCreatedAt();
+
+        this.name = board.getWriter().getName();
+        this.profileImage = board.getWriter().getProfileImage();
+
+        this.styles = board.getStyles().stream()
+                .map(OotdStyle::new)
+                .collect(Collectors.toList());
+
+        this.boardImages = board.getBoardImages().stream()
+                .map(OotdBoardImage::new)
+                .collect(Collectors.toList());
+        this.boardClothesList = board.getBoardClothesList().stream()
+                .map(OotdBoardClothesList::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class OotdStyle {
+
+        private String name;
+
+        public OotdStyle(BoardStyle boardStyle) {
+            this.name = boardStyle.getStyle().getName();
+        }
+    }
+
+    @Data
+    static class OotdBoardImage {
+
+        private String url;
+
+        public OotdBoardImage(BoardImage boardImage) {
+            this.url = boardImage.getImageUrl();
+        }
+    }
+
+    @Data
+    static class OotdBoardClothesList {
+
+        private BoardOotdGetRes.OotdBoardClothesList.ClothesBrand brand;
+
+        private String name;
+
+        private Category category;
+
+        private String size;
+
+        private String material;
+
+        private String purchaseStore;
+
+        private String purchaseDate;
+
+        public OotdBoardClothesList(BoardClothes boardClothes) {
+            Clothes clothes = boardClothes.getClothes();
+            this.brand = new BoardOotdGetRes.OotdBoardClothesList.ClothesBrand(clothes.getBrand());
+            this.name = clothes.getName();
+            this.category = clothes.getCategory();
+            this.size = clothes.getSize();
+            this.material = clothes.getMaterial();
+            this.purchaseStore = clothes.getPurchaseStore();
+            this.purchaseDate = clothes.getPurchaseDate();
+        }
+
+        @Data
+        static class ClothesBrand {
+
+            private String name;
+
+            public ClothesBrand(Brand brand) {
+                this.name = brand.getName();
+            }
+        }
+    }
+}
