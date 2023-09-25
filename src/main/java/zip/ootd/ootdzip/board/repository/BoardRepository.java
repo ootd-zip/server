f0caec8970a4622ca338400bc87313605b0c1e0f
Jinhoon Park <g22203@naver.com>
2023-09-25 15:27:20 +0900

    feat: 게시글 전체 조회 repo 추가

--- src/main/java/zip/ootd/ootdzip/board/repository/BoardRepository.java
+++ src/main/java/zip/ootd/ootdzip/board/repository/BoardRepository.java
@@ -1,7 +1,18 @@
 package zip.ootd.ootdzip.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import zip.ootd.ootdzip.board.domain.Board;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("SELECT b from Board b where b.isBlocked = false " +
+            "and b.isDeleted = false " +
+            "and b.isPublic = true " +
+            "and b.reportCount < 10 " +
+            "order by b.createdAt desc " +
+            "limit 20")
+    List<Board> findOotdAll(); // 역순
 }
