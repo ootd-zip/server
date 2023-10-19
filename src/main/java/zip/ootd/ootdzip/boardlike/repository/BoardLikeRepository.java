3e623106a776db9f4b71df2aed0c74309ae19af5
Jinhoon Park <g22203@naver.com>
2023-10-19 22:07:19 +0900

    feat: 좋아요 유저 레포지터리 생성

---
+++ src/main/java/zip/ootd/ootdzip/boardlike/repository/BoardLikeRepository.java
@@ -0,0 +1,15 @@
+package zip.ootd.ootdzip.boardlike.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import zip.ootd.ootdzip.boardlike.domain.BoardLike;
+
+public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
+
+    @Query("SELECT bl from BoardLike bl " +
+            "where bl.board.id = :boardId " +
+            "and bl.user.id = :userId ")
+    BoardLike findByBoardIdAndUserId(@Param("boardId") String boardId, @Param("userId") String userId);
+}
