b3f5fea55335c38523a6aaddfae7db73427d9b97
Jinhoon Park <g22203@naver.com>
2023-10-19 21:42:56 +0900

    feat: 북마크 유저 레포지터리 추가

---
+++ src/main/java/zip/ootd/ootdzip/boardbookmark/domain/BoardBookmark.java
@@ -0,0 +1,48 @@
+package zip.ootd.ootdzip.boardbookmark.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zip.ootd.ootdzip.board.domain.Board;
+import zip.ootd.ootdzip.common.entity.BaseEntity;
+import zip.ootd.ootdzip.user.domain.User;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardBookmark extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder.Default
+    private boolean isBookmark = false;
+
+    public static BoardBookmark createBoardBookmarkBy(User user) {
+
+        return BoardBookmark.builder()
+                .user(user)
+                .build();
+    }
+
+    public void addBookmark() {
+        this.isBookmark = true;
+    }
+
+    public void cancelBookmark() {
+        this.isBookmark = false;
+    }
+}
