f34f8de658ba03675878e1453a4ac6b53edc9760
Jinhoon Park <g22203@naver.com>
2023-10-20 15:01:02 +0900

    feat: redisKey 상수화 클래스 추가

---
+++ src/main/java/zip/ootd/ootdzip/common/constant/RedisKey.java
@@ -0,0 +1,11 @@
+package zip.ootd.ootdzip.common.constant;
+
+public class RedisKey {
+
+    public static class View {
+
+        public static final String KEY = "view";
+
+        public static final String FILTER_KEY = KEY + "filter";
+    }
+}
