1732437ea3ec57e4d84675b4e4dcbc12cac3884f
Kang Min <rkdals133@gmail.com>
2023-11-12 23:04:29 +0900

    setting : cors 세팅 추가

---
+++ src/main/java/zip/ootd/ootdzip/common/config/WebConfig.java
@@ -0,0 +1,16 @@
+package zip.ootd.ootdzip.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // TODO : 프론트 URL 확정 시 변경 필요
+                .allowedMethods("*");
+    }
+}
