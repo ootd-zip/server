1653bdbb748d95355fe272d8c8789d0b78d643c7
Kang Min <rkdals133@gmail.com>
2023-11-28 00:07:13 +0900

    style : import 정리

--- src/main/java/zip/ootd/ootdzip/clothes/domain/Clothes.java
+++ src/main/java/zip/ootd/ootdzip/clothes/domain/Clothes.java
@@ -1,6 +1,17 @@
 package zip.ootd.ootdzip.clothes.domain;
 
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +22,6 @@ import zip.ootd.ootdzip.category.domain.Size;
 import zip.ootd.ootdzip.common.entity.BaseEntity;
 import zip.ootd.ootdzip.ootdimageclothe.domain.OotdImageClothes;
 import zip.ootd.ootdzip.user.domain.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "clothes")
