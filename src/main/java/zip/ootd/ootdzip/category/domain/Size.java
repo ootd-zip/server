ab5b0805e947fb61955cc1660d8d576b66167c93
Kang Min <rkdals133@gmail.com>
2023-11-29 21:46:33 +0900

    feature : Size 엔티티에서 Category fecthtype Lazy로 변경

--- src/main/java/zip/ootd/ootdzip/category/domain/Size.java
+++ src/main/java/zip/ootd/ootdzip/category/domain/Size.java
@@ -1,6 +1,7 @@
 package zip.ootd.ootdzip.category.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class Size {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
