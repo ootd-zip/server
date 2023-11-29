fa8fdc3ebb0cde544f0b3ab98cd5b4bd51718895
hoon <g22203@naver.com>
2023-11-29 18:56:51 +0900

    feat : ootd 등록에 검사조건 추가

--- src/main/java/zip/ootd/ootdzip/ootd/data/OotdPostReq.java
+++ src/main/java/zip/ootd/ootdzip/ootd/data/OotdPostReq.java
@@ -3,6 +3,7 @@ package zip.ootd.ootdzip.ootd.data;
 import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,12 +15,13 @@ public class OotdPostReq {
 
     private List<Long> styles;
 
+    @Size(min = 1, message = "OOTD 게시글에는 반드시 1장 이상의 이미지가 있어야 합니다.")
     private List<OotdImageReq> ootdImages;
 
     @Data
     public static class OotdImageReq {
 
-        @NotEmpty(message = "이미지는 반드시 1장 이상이여야 합니다.")
+        @NotEmpty(message = "OOTD 이미지는 반드시 존재해야 합니다.")
         private String ootdImage;
 
         private List<ClothesTagReq> clothesTags;
