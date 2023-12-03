ed48a321f1f327576fdeee1c9af72e99d0a51261
Kim Kang Min <rkdals133@gmail.com>
2023-12-03 21:19:55 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-chmod 755 /home/ubuntu/server/build/libs/ootdzip-0.0.1-SNAPSHOT.jar
-nohup java -jar /home/ubuntu/server/build/libs/ootdzip-0.0.1-SNAPSHOT.jar &
+PROJECT_ROOT="/home/ubuntu/server" #코드가 주입되는 경로
+JAR_FILE="$PROJECT_ROOT/ootdzip-0.0.1-SNAPSHOT.jar" #build.gradle에서 설정한 파일명으로 변경
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
+
