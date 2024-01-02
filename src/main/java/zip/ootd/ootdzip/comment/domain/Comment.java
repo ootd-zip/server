065b0b26057d752fcfefab542728a562ad6d7cc1
hoon <g22203@naver.com>
2024-01-02 21:25:46 +0900

    feat : Comment 도메인 수정

--- src/main/java/zip/ootd/ootdzip/comment/domain/Comment.java
+++ src/main/java/zip/ootd/ootdzip/comment/domain/Comment.java
@@ -1,17 +1,20 @@
-package zip.ootd.ootdzip.comment;
+package zip.ootd.ootdzip.comment.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import zip.ootd.ootdzip.common.entity.BaseEntity;
 import zip.ootd.ootdzip.ootd.domain.Ootd;
 import zip.ootd.ootdzip.user.domain.User;
@@ -19,26 +22,46 @@ import zip.ootd.ootdzip.user.domain.User;
 @Entity
 @Table(name = "Comments")
 @Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment extends BaseEntity {
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ootd_id", nullable = false)
     private Ootd ootd;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User writer;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User taggedUser;
+
+    @Builder.Default
+    private int depth = 0;
+
+    @Builder.Default
+    private int report = 0;
+
     private String contents;
 
+    @Builder.Default
     @Column(nullable = false)
     private Boolean isDeleted = false;
 
-    @ManyToOne
-    @JoinColumn(name = "parent_id", nullable = true)
-    private Comment parent;
+    @Builder.Default
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent = null;
 
+    @Builder.Default
     @OneToMany(mappedBy = "parent")
-    private List<Comment> children = new ArrayList<>();
+    private List<Comment> childComments = new ArrayList<>();
+
+    public void addChildComment(Comment comment) {
+        childComments.add(comment);
+        comment.setParent(this);
+    }
 }
