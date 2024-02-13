5066403ceb9a7825c76ef4ad54131a22eb820e3a
Kang Min <rkdals133@gmail.com>
2024-02-13 23:06:33 +0900

    fix: 스타일 수정

--- src/main/java/zip/ootd/ootdzip/clothes/controller/ClothesController.java
+++ src/main/java/zip/ootd/ootdzip/clothes/controller/ClothesController.java
@@ -1,14 +1,21 @@
 package zip.ootd.ootdzip.clothes.controller;
 
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
 import zip.ootd.ootdzip.clothes.controller.request.FindClothesByUserRequestCommon;
 import zip.ootd.ootdzip.clothes.controller.request.SaveClothesReq;
 import zip.ootd.ootdzip.clothes.data.DeleteClothesByIdRes;
@@ -17,8 +24,6 @@ import zip.ootd.ootdzip.clothes.data.SaveClothesRes;
 import zip.ootd.ootdzip.clothes.service.ClothesService;
 import zip.ootd.ootdzip.common.response.ApiResponse;
 import zip.ootd.ootdzip.user.service.UserService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
