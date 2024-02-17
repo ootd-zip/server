ae34c9188623c9955b67942f6333f7f052aafbf8
Kang Min <rkdals133@gmail.com>
2024-02-17 22:52:49 +0900

    style : 변수명 수정

--- src/main/java/zip/ootd/ootdzip/clothes/controller/request/FindClothesByUserReq.java
+++ src/main/java/zip/ootd/ootdzip/clothes/controller/request/FindClothesByUserReq.java
@@ -6,7 +6,7 @@ import zip.ootd.ootdzip.clothes.service.request.FindClothesByUserSvcReq;
 import zip.ootd.ootdzip.common.request.CommonPageRequest;
 
 @Setter
-public class FindClothesByUserRequestCommon extends CommonPageRequest {
+public class FindClothesByUserReq extends CommonPageRequest {
 
     @Positive(message = "유저 ID는 양수여야 합니다.")
     private Long userId;
