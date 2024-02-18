63cefb20911288c54ee2bb2774e5caf79ab39ccf
Kang Min <rkdals133@gmail.com>
2024-02-18 11:24:43 +0900

    feature : 옷 수정에 대한 컨트롤러 테스트 추가

--- src/main/java/zip/ootd/ootdzip/clothes/controller/request/UpdateClothesReq.java
+++ src/main/java/zip/ootd/ootdzip/clothes/controller/request/UpdateClothesReq.java
@@ -2,40 +2,56 @@ package zip.ootd.ootdzip.clothes.controller.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import zip.ootd.ootdzip.clothes.data.PurchaseStoreType;
 import zip.ootd.ootdzip.clothes.service.request.UpdateClothesSvcReq;
+import zip.ootd.ootdzip.common.valid.EnumValid;
 
 @Getter
 @NoArgsConstructor
 public class UpdateClothesReq {
 
+    @NotBlank(message = "구매처는 필수입니다.")
     private String purchaseStore;
 
+    @EnumValid(enumClass = PurchaseStoreType.class, message = "유효하지 않은 구매처 타입입니다.")
     private PurchaseStoreType purchaseStoreType;
 
+    @Positive(message = "브랜드 ID는 양수여야 합니다.")
     private Long brandId;
 
+    @Positive(message = "카테고리 ID는 양수여야 합니다.")
     private Long categoryId;
 
-    private List<Long> colorIds;
+    @NotEmpty(message = "색은 필수입니다.")
+    private List<@Positive(message = "색 ID는 양수여야 합니다.") Long> colorIds;
 
+    @NotNull(message = "공개여부는 필수입니다.")
     private Boolean isOpen;
 
+    @Positive(message = "사이즈 ID는 양수여야 합니다.")
     private Long sizeId;
 
+    @NotBlank(message = "이미지는 필수입니다.")
     private String clothesImageUrl;
 
+    @Size(max = 2001, message = "메모는 최대 2000자입니다.")
     private String memo;
 
+    @NotBlank(message = "제품명은 필수입니다.")
     private String name;
 
     private String purchaseDate;
 
     @Builder
-    public UpdateClothesReq(String purchaseStore, PurchaseStoreType purchaseStoreType, Long brandId, Long categoryId,
+    private UpdateClothesReq(String purchaseStore, PurchaseStoreType purchaseStoreType, Long brandId, Long categoryId,
             List<Long> colorIds, Boolean isOpen, Long sizeId, String clothesImageUrl, String memo, String name,
             String purchaseDate) {
         this.purchaseStore = purchaseStore;
