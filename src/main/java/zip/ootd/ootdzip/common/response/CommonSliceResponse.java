373b947cf15d884178dc39a57f57d90f52407939
hoon <g22203@naver.com>
2024-02-19 23:44:00 +0900

    style : style 수정

--- src/main/java/zip/ootd/ootdzip/common/response/CommonSliceResponse.java
+++ src/main/java/zip/ootd/ootdzip/common/response/CommonSliceResponse.java
@@ -1,10 +1,10 @@
 package zip.ootd.ootdzip.common.response;
 
-import lombok.Data;
+import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import lombok.Data;
 
 @Data
 public class CommonSliceResponse<T> {
