4e61176bbcc757f3e1dcf3a59f7fec4dc66f9eb5
Lechros <0onionion0@gmail.com>
2024-03-28 00:20:16 +0900

    feat: RegisteredOAuth2UserService 추가
    OAuth2UserRequest로부터 사용자를 찾거나 생성하여 RegisteredOAuth2User를 반환합니다.

---
+++ src/main/java/zip/ootd/ootdzip/login/service/RegisteredOAuth2UserService.java
@@ -0,0 +1,61 @@
+package zip.ootd.ootdzip.login.service;
+
+import java.util.Optional;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import zip.ootd.ootdzip.login.data.RegisteredOAuth2User;
+import zip.ootd.ootdzip.login.domain.UserSocialLogin;
+import zip.ootd.ootdzip.login.repository.UserSocialLoginRepository;
+import zip.ootd.ootdzip.user.domain.User;
+import zip.ootd.ootdzip.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RegisteredOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+    private final UserSocialLoginRepository userSocialLoginRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 토큰 정보 API에서 사용자 정보 받아오기
+        OAuth2User oauth2User = super.loadUser(userRequest);
+        // 받아온 정보의 sub로 필요 시 DB에 저장하고 user 가져오기
+        User user = findOrRegister(userRequest, oauth2User);
+        // name이 user.id로 설정된 OAuth2User 반환
+        return RegisteredOAuth2User.from(oauth2User, user);
+    }
+
+    private User findOrRegister(OAuth2UserRequest userRequest, OAuth2User oauth2User) {
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String providerId = oauth2User.getName();
+
+        return find(provider, providerId).orElseGet(() -> register(provider, providerId));
+    }
+
+    private Optional<User> find(String provider, String providerId) {
+        return userSocialLoginRepository
+                .findByProviderAndProviderId(provider, providerId)
+                .map(UserSocialLogin::getUser);
+    }
+
+    private User register(String provider, String providerId) {
+        User user = User.getDefault();
+        user = userRepository.save(user);
+
+        UserSocialLogin socialLogin = UserSocialLogin.builder()
+                .user(user)
+                .provider(provider)
+                .providerId(providerId)
+                .build();
+        socialLogin = userSocialLoginRepository.save(socialLogin);
+
+        return user;
+    }
+}
