1eac4cc47ea0f322271272ddabbd826b4341f951
hoon <g22203@naver.com>
2024-06-13 01:40:41 +0900

    fix : swagger 가 나타나지 않은 문제 수정

--- src/main/java/zip/ootd/ootdzip/common/config/RedisConfig.java
+++ src/main/java/zip/ootd/ootdzip/common/config/RedisConfig.java
@@ -59,8 +59,10 @@ public class RedisConfig {
                 .entryTtl(Duration.ofMinutes(timeout));
     }
 
-    // jackson LocalDateTime mapper
-    @Bean
+    /**
+     * redis 전용 mapper, 기본적인 mapper 는 time, 클래스타입 변환 문제가 있어 아래 전용 mapper 를 통해 해결
+     * Bean 으로 설정시 전체 적용되어 swagger 에서도 해당 mapper 를 사용하게되어 예상치않은 에러가 발생하므로 빈으로 쓰지말것
+     */
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // timestamp 형식 안따르도록 설정
