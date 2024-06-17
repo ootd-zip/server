6e5e2386563b547f2307a742cc6e312d9c43d2b9
Kang Min <rkdals133@gmail.com>
2024-06-18 00:43:13 +0900

    cicd: dev 환경 분리를 위한 스크립트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# AWS CodeDeploy 애플리케이션 이름
+APPLICATION_NAME="ootdzip-CD"
+
+# 현재 배포 그룹 이름을 가져옵니다.
+DEPLOYMENT_GROUP_NAME=$(aws deploy get-deployment-group --region ap-northeast-2 --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --query "deploymentGroupInfo.deploymentGroupName" --output text)
+
+# 배포 그룹에 따라 대상 디렉토리를 설정합니다.
+if [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-cd-group" ]; then
+    JAR_OPTION = "--spring.profiles.active=prod"
+elif [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-cd-dev" ]; then
+    JAR_OPTION = "--spring.profiles.active=dev"
+else
+    echo "Unknown deployment group: $DEPLOYMENT_GROUP_NAME"
+    exit 1
+fi
+
+#!/usr/bin/env bash
+
 PROJECT_ROOT="/home/ubuntu/server" #코드가 주입되는 경로
 JAR_FILE="$PROJECT_ROOT/build/libs/ootdzip-0.0.1-SNAPSHOT.jar" #build.gradle에서 설정한 파일명으로 변경
 
@@ -10,7 +28,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE $JAR_OPTION > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
