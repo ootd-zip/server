f7844f8c1b255e7c21ed96b2b18ba43aaf7e6bd3
Kang Min <rkdals133@gmail.com>
2024-06-18 00:52:25 +0900

    cicd: dev 환경 분리를 위한 스크립트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,6 +20,7 @@ fi
 
 PROJECT_ROOT="/home/ubuntu/server" #코드가 주입되는 경로
 JAR_FILE="$PROJECT_ROOT/build/libs/ootdzip-0.0.1-SNAPSHOT.jar" #build.gradle에서 설정한 파일명으로 변경
+JAR_COMMAND = "$JAR_FILE $JAR_OPTION"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -27,8 +28,8 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE $JAR_OPTION > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW > $JAR_COMMAND 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_COMMAND > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
