3523dba4d4bab3af334108fdba2ea326fea84181
Kang Min <rkdals133@gmail.com>
2024-06-17 23:12:50 +0900

    cicd: dev 환경 분리를 위한 스크립트 변경

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# AWS CodeDeploy 애플리케이션 이름
-APPLICATION_NAME="ootdzip-CD"
-
-# 현재 배포 그룹 이름을 가져옵니다.
-DEPLOYMENT_GROUP_NAME=$(aws deploy get-deployment-group --region ap-northeast-2 --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --query "deploymentGroupInfo.deploymentGroupName" --output text)
-
-# 배포 그룹에 따라 대상 디렉토리를 설정합니다.
-if [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-cd-group" ]; then
-    DESTINATION_DIR="/home/ubuntu/main"
-elif [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-dev-group" ]; then
-    DESTINATION_DIR="/home/ubuntu/develop"
-else
-    echo "Unknown deployment group: $DEPLOYMENT_GROUP_NAME"
-    exit 1
-fi
-
-chmod 755 $DESTINATION_DIR/scripts/start.sh
-chmod 755 $DESTINATION_DIR/scripts/stop.sh
+chmod 755 /home/ubuntu/server/scripts/start.sh
+chmod 755 /home/ubuntu/server/scripts/stop.sh
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 
-# AWS CodeDeploy 애플리케이션 이름
-APPLICATION_NAME="ootdzip-CD"
-
-# 현재 배포 그룹 이름을 가져옵니다.
-DEPLOYMENT_GROUP_NAME=$(aws deploy get-deployment-group --region ap-northeast-2 --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --query "deploymentGroupInfo.deploymentGroupName" --output text)
-
-# 배포 그룹에 따라 대상 디렉토리를 설정합니다.
-if [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-cd-group" ]; then
-    PROJECT_ROOT="/home/ubuntu/main"
-    JAR_FILE_NAME = "ootdzip-prod.jar"
-elif [ "$DEPLOYMENT_GROUP_NAME" == "ootdzip-dev-group" ]; then
-    PROJECT_ROOT="/home/ubuntu/develop"
-    JAR_FILE_NAME = "ootdzip-dev.jar"
-else
-    echo "Unknown deployment group: $DEPLOYMENT_GROUP_NAME"
-    exit 1
-fi
-
-JAR_FILE="$PROJECT_ROOT/build/libs/$JAR_FILE_NAME" #build.gradle에서 설정한 파일명으로 변경
+PROJECT_ROOT="/home/ubuntu/server" #코드가 주입되는 경로
+JAR_FILE="$PROJECT_ROOT/build/libs/ootdzip-0.0.1-SNAPSHOT.jar" #build.gradle에서 설정한 파일명으로 변경
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-PID_FILE = "$PROJECT_ROOT/PID.log"
 
 TIME_NOW=$(date +%c)
 
@@ -32,5 +14,3 @@ nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
-
-
