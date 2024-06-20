7e8b758a420a287a636978e713d76abf1ac4c87d
hoon <g22203@naver.com>
2024-06-20 19:18:02 +0900

    fix : 오타 수정

--- src/main/java/zip/ootd/ootdzip/lock/domain/RLockType.java
+++ src/main/java/zip/ootd/ootdzip/lock/domain/RLockType.java
@@ -10,7 +10,7 @@ public enum RLockType {
     OOTD_VIEW_COUNT("ootdViewCount" + ":RLock:%s", 2L, 5L),
     OOTD_LIKE_COUNT("ootdLikeCount" + ":RLock:%s", 2L, 5L),
     OOTD_BOOKMARK_COUNT("ootdBookmarkCount" + ":RLock:%s", 2L, 5L),
-    REPORT_COUNT("reportCount" + ":RLock:%s", 2L, 5L),
+    REPORT_COUNT("reportCount" + ":RLock:%s", 2L, 5L);
 
     private final String format;
     private final Long waitSecond; // lock 을 얻기 위해 기다리는 시간
@@ -19,4 +19,4 @@ public enum RLockType {
     public String getLockKey(String key) {
         return String.format(getFormat(), key);
     }
-    }
+}
