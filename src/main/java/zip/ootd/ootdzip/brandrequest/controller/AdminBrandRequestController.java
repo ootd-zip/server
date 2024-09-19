f8ed83cf50d9bed4238c3214909b9aace92151eb
Kang Min <rkdals133@gmail.com>
2024-09-19 22:51:30 +0900

    fix : 브랜드 요청 조회 수정

--- src/main/java/zip/ootd/ootdzip/brandrequest/controller/AdminBrandRequestController.java
+++ src/main/java/zip/ootd/ootdzip/brandrequest/controller/AdminBrandRequestController.java
@@ -1,11 +1,14 @@
 package zip.ootd.ootdzip.brandrequest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.*;
-
 import zip.ootd.ootdzip.brandrequest.controller.reqeuest.BrandRequestApproveReq;
 import zip.ootd.ootdzip.brandrequest.controller.reqeuest.BrandRequestRejectReq;
 import zip.ootd.ootdzip.brandrequest.controller.reqeuest.BrandRequestSearchReq;
@@ -38,7 +41,7 @@ public class AdminBrandRequestController {
 
     @GetMapping
     public ApiResponse<CommonPageResponse<BrandRequestSearchRes>> searchBrandRequest(
-            @Valid @RequestBody BrandRequestSearchReq request) {
+            @Valid BrandRequestSearchReq request) {
         return new ApiResponse<>(brandRequestService.searchBrandRequest(request.toServiceRequest(),
                 userService.getAuthenticatiedUser()));
     }
